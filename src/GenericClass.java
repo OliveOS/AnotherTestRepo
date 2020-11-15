02012e835ee68f334c39d27a04d7c397a4a5c50d
Oliver Wooding <oliverwooding@icloud.com>
2020-11-15 21:22:37 +0100

    initial commit

---
+++ src/GenericClass.java
@@ -0,0 +1,25 @@
+public class GenericClass {
+    private int number;
+    private String word;
+
+    public GenericClass(int number, String word) {
+        this.number = number;
+        this.word = word;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
