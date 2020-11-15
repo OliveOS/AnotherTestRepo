7471f31c4e23cfa115b0c753565f085073a8c37a
Oliver Wooding <oliverwooding@icloud.com>
2020-11-15 21:42:46 +0100

    new random class

---
+++ src/SomeClass.java
@@ -0,0 +1,35 @@
+public class SomeClass {
+    private int data;
+    private String word;
+    private char val;
+
+    public SomeClass(int data, String word, char val) {
+        this.data = data;
+        this.word = word;
+        this.val = val;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public char getVal() {
+        return val;
+    }
+
+    public void setVal(char val) {
+        this.val = val;
+    }
+}
